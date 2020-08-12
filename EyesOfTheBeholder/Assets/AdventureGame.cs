1f6c17021be9acf0120d5f7e86756a3b2e684b29
Otutakhil <tutakho@gmail.com>
2020-08-11 22:16:55 -0700

    Initial Commit, Text101 26

---
+++ EyesOfTheBeholder/Assets/AdventureGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponet;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponet.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
